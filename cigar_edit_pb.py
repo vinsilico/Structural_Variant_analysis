44a9e5fa36a5fcec425c5d9e13743602ceb4eeff
Vinoy <77330794+vinsilico@users.noreply.github.com>
2022-07-29 06:59:21 +0530

    Update cigar_edit_pb.py

--- cigar_edit_pb.py
+++ cigar_edit_pb.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-# Script gets a imput samfile with "N" in the CIGAR string, replace "N" with "D"
+# when splice option is used with minimap2, minimap2 uses N in the Cigar string, which wont work with SV caller.
+# so, N in the CIGAR string need to be changed to D
+# This script gets a samfile with "N" in the CIGAR string, and writes a new sam file with "D" instead of "N"
+# This script also writes the original and modified CIGAR string in a text file, for reference.
 
 import sys
 
